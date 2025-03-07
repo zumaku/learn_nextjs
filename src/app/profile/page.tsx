f613788acc394d647160c4b05c0b6aec53b8754a
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-08 05:37:54 +0800

    Static Meta Data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+    title: "This is Profile Page",
+    description: "This meta data from page.tsx"
+}
+
 export default function Profile() {
     return <h1>Profile Page!</h1>
 };
