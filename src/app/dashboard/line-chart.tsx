81fc8260e3b4e461e68206b7c8ce88275cac7c8b
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-07 19:20:06 +0800

    File Collection

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,3 @@
+export default function LineChart() {
+    return <h1>This is Line Chart!</h1>
+};
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+function BarChart() {
+    return <h1>This is Bar Chart!</h1>
+}
+
+export default function Dashboard() {
+    return <h1>Dashboard Page!</h1>
+};
