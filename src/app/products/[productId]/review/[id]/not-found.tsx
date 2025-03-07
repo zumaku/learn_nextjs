4df8a2edef17f9afe2638117aabde572c14a34f2
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-07 19:00:17 +0800

    not-found with usePathname

--- src/app/products/[productId]/review/[id]/not-found.tsx
+++ src/app/products/[productId]/review/[id]/not-found.tsx
@@ -1,3 +1,11 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+
 export default function NotFound() {
-    return <h1>Sorry, it`s 404. Review Not Found!</h1>
+    const pathName = usePathname()
+    const productId = pathName.split("/")[2]
+    const reviewId = pathName.split("/")[4]
+
+    return <h1>Sorry, it`s 404. Product {productId} with Review Id {reviewId}</h1>
 };
