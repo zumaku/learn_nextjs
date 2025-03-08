b9ee4cb589f6e964440d6b8ae1ec2ad901cd6090
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-08 14:31:37 +0800

    Link Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link"
+
 export default function Home(){
-    return <h1>Welcome Home!</h1>
+    return (
+        <>
+            <h1>Welcome Home!</h1>
+            <Link href={"/blogs"}>Blogs</Link>
+            <Link href={"/products"}>Products</Link>
+            <Link href={"/profile"}>Profile</Link>
+        </>
+    )
 }
