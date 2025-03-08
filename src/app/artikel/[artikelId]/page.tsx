0aaae2c8f9be34c860f18147acb6e3261868733e
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-08 21:40:15 +0800

    searchParams Cara 3. Cara lain jika dirender di client. Ini Best Practice nya

--- src/app/artikel/[artikelId]/page.tsx
+++ src/app/artikel/[artikelId]/page.tsx
@@ -1,16 +1,13 @@
-"use client"
-
-import { use } from "react"
-
-export default function ArtikelDetial({params, searchParams}: {
-    params: Promise<{artikelId: string}>,
-    searchParams: Promise<{sembarang?: string}>
-}) {
-    const {artikelId} = use(params)
-    const {sembarang = "id"} = use(searchParams)
-
-    return(<>
-        <h1>Artikel Detail</h1>
-        <p>Dengan `params` ({artikelId}) dan `searchParams` ({sembarang})</p>
-    </>)
-};
+'use client'
+ 
+import { useSearchParams } from 'next/navigation'
+ 
+export default function SearchBar() {
+  const searchParams = useSearchParams()
+ 
+  const search = searchParams.get('search')
+ 
+  // URL -> `/dashboard?search=my-project`
+  // `search` -> 'my-project'
+  return <>Search: {search}</>
+}
