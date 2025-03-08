676495344b844193b7f083e116ed5007459d7803
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-08 13:56:27 +0800

    Title Metadata: default, template, & absolute

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: {absolute: "Absolute Product"}
+}
+
 export default function Prodducts() {
     return <h1>Products Page</h1>
 };
