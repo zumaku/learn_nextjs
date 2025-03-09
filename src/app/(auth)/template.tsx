9221129e3676da668f67b28df4d287ac09c517b4
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-09 11:19:36 +0800

    Template

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -3,6 +3,7 @@
 import "./style.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 type Navlinks = {
   name: string, href: string
@@ -19,9 +20,13 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [input, setInput] = useState("")
   const pathName = usePathname()
   return (
     <>
+      <div className="">
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
       {navLinks.map((link) => (
         <Link key={link.name} href={link.href}>
             <h2 className={pathName === link.href && pathName !== "/" ? `font-bold text-lg underline` : `text-md underline`}>{link.name}</h2>
