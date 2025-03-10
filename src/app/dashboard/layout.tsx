6cdc12cfa5ec4af63965af20357baaee1477e5e7
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-10 12:50:11 +0800

    Conditional Route

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ type DashboardType = {
     children: React.ReactNode,
     barchart: React.ReactNode,
     linechart: React.ReactNode,
+    piechart: React.ReactNode,
     notifications: React.ReactNode
 }
 
@@ -9,10 +10,12 @@ export default function LayoutDashboard({
     children,
     barchart,
     linechart,
+    piechart,
     notifications
 }: DashboardType) {
+    const isAdmin = true
     return (
-        <div className="w-full h-screen flex flex-col">
+        <div className="w-full flex flex-col">
             <div className="w-full mb-3">{children}</div>
             <div className="flex">
                 <div className="flex flex-col w-1/2">
@@ -21,6 +24,7 @@ export default function LayoutDashboard({
                 </div>
                 <div className="w-1/2">{notifications}</div>
             </div>
+            {isAdmin && <div className="w-full">{piechart}</div>}
         </div>
     );
 }
