f01847f935e0fc82002405c09c31a3f8c06d7a6e
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-11 11:24:19 +0800

    GET and POST request

---
+++ src/app/api/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id: 1,
+        text: "Thi is the first commant"
+    },
+    {
+        id: 2,
+        text: "Thi is the second commant"
+    },
+    {
+        id: 3,
+        text: "Thi is the third commant"
+    }
+]
