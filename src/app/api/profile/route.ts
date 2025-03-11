2aaf8ac9e83a591a86dfd66841df5dcc6bc969d8
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-11 14:18:01 +0800

    Cookies in req & res

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,12 +1,19 @@
-import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 
-export async function GET(req: NextRequest) {
-    const reqHeader = new Headers(req.headers)
-    console.log(reqHeader)
-    
+export async function GET(){
+
+    // Get cookie on request
+    const cookie = await cookies()
+    const theme = cookie.get("theme")
+    console.log(theme)
+
+    // <== Set cookie on Response way 1
+    cookie.set("secret-key", "balala")
+
     return new Response("<h1>Profile API data</h1>.", {
         headers: {
-            "Conten-Type": "text/html"
+            "Conten-Type": "text/html",
+            "Set-cookie": "theme=dart"      // <== Set cookie on Response way 2
         }
     })
 }
