9e8a42f12a6922438bf907e148d3ff4581ac77f4
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-11 15:18:45 +0800

    Middleware Coocie & Costum Headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,20 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-// Ini untuk melakukan redirectnya
 export function middleware(request: NextRequest) {
-    return NextResponse.rewrite(new URL('/hello', request.url))
+    const response = NextResponse.next()
+    const themeData = request.cookies.get("theme")
+
+    if(!themeData) response.cookies.set("theme", "dark")
+
+    response.headers.set("costum-header", "costum-value")
+
+    return response
+
+
+    // return NextResponse.rewrite(new URL('/hello', request.url))
 }
 
-// Ini untuk menentukan url mana saja yang akan kena redirect
-export const config = {
-    matcher: '/profile',
-}
+// export const config = {
+//     matcher: '/profile',
+// }
