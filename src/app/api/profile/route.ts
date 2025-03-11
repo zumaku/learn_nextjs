aa18b049a6bbec089bd8dfaf9eabecf5eeee9bf2
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-11 13:54:24 +0800

    Costum Response Header

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,12 +1,12 @@
-import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-    // const reqHeader = new Headers(req.headers)
-    // console.log(reqHeader)
+    const reqHeader = new Headers(req.headers)
+    console.log(reqHeader)
     
-    const headList = await headers()
-    console.log(headList.get("Authorization"))
-    
-    return new Response("Profile API data.")
+    return new Response("<h1>Profile API data</h1>.", {
+        headers: {
+            "Conten-Type": "text/html"
+        }
+    })
 }
