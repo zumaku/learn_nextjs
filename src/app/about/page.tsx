9db16b5d09c4810ec6a9360cdaf17bbe83e6587d
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-13 21:24:23 +0800

    Dynamic Rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,11 @@
-export default function DashboardPage() {
+import { cookies } from "next/headers";
+
+export default async function DashboardPage() {
+    const cookieStore = await cookies()
+    const theme = cookieStore.get("theme")
+    console.log(theme)
+    console.log("Dynamic Rendering")
+
     return (
         <h1>Dashboard Page {new Date().toLocaleTimeString()}</h1>
     );
