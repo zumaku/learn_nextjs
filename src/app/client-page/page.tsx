e853393a2fdee644726041fc49c986ee990b5d0b
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-14 18:53:15 +0800

    Client Olny Code

--- src/app/client-page/page.tsx
+++ src/app/client-page/page.tsx
@@ -1,10 +1,16 @@
-// app/client-route/page.tsx
 "use client";
 
 import { useTheme } from "@/components/ThemeProvider";
+import { ClientFuncionExample } from "@/utils/clientFunctionExample";
 
 export default function ClientRoutePage() {
   const theme = useTheme();
+  const result = ClientFuncionExample()
 
-  return <h1 style={{ color: theme.colors.secondary }}>Client Route Page</h1>;
+  return (
+    <>
+      <h1 style={{ color: theme.colors.secondary }}>Client Route Page</h1>
+      <p>{result}</p>
+    </>
+  )
 }
---
+++ src/utils/clientFunctionExample.ts
@@ -0,0 +1,10 @@
+import "client-only"
+
+export function ClientFuncionExample() {
+    console.log(`
+        Manipulasi DOM.
+        Akses Local Storage.
+        dll.
+    `)
+    return "Ini Client Function"
+}
