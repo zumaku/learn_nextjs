c2a7102b5ccf9761da99cc15ba763e0d3b27469d
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-15 09:24:06 +0800

    Data Fetching in Server Component

---
+++ src/app/users-server/page.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+const fetchUsers = async (): Promise<User[]> => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  if (!res.ok) throw new Error("Failed to fetch users");
+  return res.json();
+};
+
+const UsersServer: FC = async () => {
+  const users = await fetchUsers();
+
+  return (
+    <ul className="flex flex-wrap justify-start gap-2">
+      {users.map((user) => (
+        <li key={user.id} className="p-2 border rounded-xl" >
+          <h3>{user.name} ({user.username})</h3>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UsersServer;
