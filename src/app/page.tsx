ad5b997a80388cd1540a21744c04d3123e9332cc
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-03-16 16:51:52 +0800

    Form Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Search from "@/components/Search";
+
+
 export default function Home(){
     return (
         <div className="flex flex-col">
             <h1>Welcome Home!</h1>
+            <Search />
         </div>
     )
 }
--- src/app/products-db/page.tsx
+++ src/app/products-db/page.tsx
@@ -8,8 +8,9 @@ export type Products = {
   description: string | null;
 };
 
-export default async function ProductsPage() {
-  const products: Products[] = await getProducts();
+export default async function ProductsPage({searchParams}: {searchParams: Promise<{query?: string}>}) {
+  const {query} = await searchParams
+  const products: Products[] = await getProducts(query);
 
   return <ProductDetail products={products} />
 }
